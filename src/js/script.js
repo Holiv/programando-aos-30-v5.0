84a3f80734e36f303c70c69efe430ec87e063684
holiv <mrheltonso@gmail.com>
2022-09-28 00:20:52 -0300

    criação de menu toggler animated icon

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,13 @@ const totalHeight = document.querySelector("html").offsetHeight;
 const articles = document.querySelectorAll('.articles');
 const popupBg = document.querySelector('#popup_bg');
 const branchImg = document.querySelector('#branch');
+const toggleMenu = document.querySelector('#toggle_menu');
+const toggleBar = document.querySelectorAll('.toggle_bar');
+const styleDisplayNone = `style.display = "none"`
+const styleDisplayBlock = `style.display = "block"`
 
 
-const shrink = () => {
+const articlesCarrousel = () => {
 
     setTimeout(() => {
         articles[0].classList.add("shrink");
@@ -46,17 +50,18 @@ const shrink = () => {
         }, 12000);
 
     }, 16000);
-
-
 }
 
-const preConfig = () => {
-    shrink();
-    popupBg.style.height = "200vh";
-    popupBg.style.border = "none";
-}
-window.onload = preConfig();
+window.onload = articlesCarrousel();
 
+//display functions
+const displayNone = elements => {
+    elements.style.display = "none"
+}
+
+const displayBlock = elements => {
+    elements.style.display = "block"
+}
 
 // popup configuration
 const popup = document.querySelector('#popup_introduction');
@@ -75,11 +80,45 @@ close_button.addEventListener('click', function(){
     popup.style.animation = "fadeaway 1s ease";
     popup_bg.style.animation = "bg_fadeaway 1s ease";
     close_button.classList.add("click_button");
-    // close_button.style.transform = "translate(2px, 2px)";
-    // close_button.style.box-shadow = "0px 0px 0px transparent";
+
     setTimeout(() => {
-        popup.style.display = "none";
-        popup_bg.style.display = "none";
-        branchImg.style.display = "block"
+        displayNone(popup);
+        displayNone(popup_bg);
+
+        displayBlock(branchImg);
     }, 1000);
 });
+
+// side menu and menu toggler button config
+const rotateFunctionAdd = (rotateElementPositive, rotateElementNegative) => {
+    rotateElementPositive.classList.add("toggle_01");
+    rotateElementPositive.classList.remove("toggle_02_0");
+    rotateElementNegative.classList.add("toggle_02");
+    rotateElementNegative.classList.remove("toggle_01_0");
+
+    toggleBar[1].classList.add("fadeaway");
+    setTimeout(() => {
+        displayNone(toggleBar[1]);
+    }, 950);
+}
+
+const rotateFunctionRemove = (rotateElementPositive, rotateElementNegative) => {
+    rotateElementPositive.classList.remove("toggle_01");
+    rotateElementNegative.classList.remove("toggle_02");
+    
+    toggleBar[1].classList.remove("fadeaway");
+    setTimeout(() => {
+        displayBlock(toggleBar[1]);
+    }, 50);
+}
+
+const showSideMenu = () => {
+    const menuBar01 = document.querySelector(".toggle_01");
+    const menuBar02 = document.querySelector(".toggle_02");
+    console.log(menuBar01, menuBar02)
+
+    menuBar01 ? rotateFunctionRemove(toggleBar[0], toggleBar[2]) : rotateFunctionAdd(toggleBar[0], toggleBar[2]);
+}
+
+toggleMenu.addEventListener("click", showSideMenu);
+
