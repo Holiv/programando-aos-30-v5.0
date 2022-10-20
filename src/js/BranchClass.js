c98e07196f67b6ad1601c7b4af37bed2a2cc0fbb
mdwiltfong <wiltfong.michael1@gmail.com>
2022-10-20 11:14:45 -0400

    initial draft of Branch class

--- src/js/BranchClass.js
+++ src/js/BranchClass.js
@@ -1,8 +1,22 @@
 class Branch {
-  branchImg;
-  branchWidth;
-  element;
-  branchFactorTop;
-  branchFactorLeft;
-  constructor() {}
+  #branchImg;
+  #branchWidth;
+  #totalWidth;
+  #commits = [];
+  constructor() {
+    this.branchImg = document.querySelector("#branch");
+    this.branchWidth = document.querySelector("#branch_img").width;
+    this.totalWidth = document.querySelector("html").offsetWidth;
+    this.renderCommits();
+  }
+  // Function to add new commits to the branch.
+  putCommit(CommitObject) {
+    this.#commits.push(CommitObject);
+  }
+  //Function to render all the commits object in the #commits array.
+  renderCommits() {
+    this.#commits.forEach((Commit) => {
+      Commit.commitPosition();
+    });
+  }
 }
